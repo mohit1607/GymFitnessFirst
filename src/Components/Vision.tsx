c41aa502d8e8e17a2dc13b04ccc3baff3373808f
Mohit Ashiliya <mohit.ashliya@gmail.com>
2022-03-06 22:01:50 +0530

    white background vanished. improved ui

--- src/Components/Vision.tsx
+++ src/Components/Vision.tsx
@@ -11,7 +11,7 @@ export const Vision = () => {
            </div>
           </div>
           <div className="col">
-             <p>I jsut want to show you folks that Gym is not only the solution for fitness. It is a tool. Not Everyone is meant for discipline. I always make sure to harness the power of discipline first. Not Everyone can become body-builder but everyone can become a fit person. Fitness is a life style not temporary work.
+             <p className='visionText'>I jsut want to show you folks that Gym is not only the solution for fitness. It is a tool. Not Everyone is meant for discipline. I always make sure to harness the power of discipline first. Not Everyone can become body-builder but everyone can become a fit person. Fitness is a life style not temporary work.
                So I am improving things by my own actions want as many folks as possible to be fit and living life on their fullest. The Goddess of opportunity is availible to all but only who are ready can get benefit.
                So try on your own and spread the word.
              </p>
